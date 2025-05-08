98f15b10e82c0cec7e82f817a5f8d6952a288e0d
Yassin Khalifa <yassin.amer@live.com>
2025-05-08 11:06:48 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='config_manager',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[],
+    description='Dynamic JupyterHub config manager service',
+    author='Pitt Center for Research Computing and Data',
+    author_email='crc@pitt.edu',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ]
+)
---
+++ tests/test_manager.py
@@ -0,0 +1,19 @@
+import os
+import tempfile
+import json
+from config_manager import manager
+
+
+def test_write_and_read_cache():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        cache_file = os.path.join(tmpdir, "cache.json")
+        venvs = ['env1', 'env2']
+        modules = {'mod1': {'modules': ['a']}}
+
+        manager.write_cache(cache_file, venvs, modules)
+
+        with open(cache_file) as f:
+            data = json.load(f)
+
+        assert data['virtualenvs'] == venvs
+        assert data['modules'] == modules
