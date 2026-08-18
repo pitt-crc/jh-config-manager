aab43a37825e1b4ed797ac872a1f483c9a4e13a3
Daniel Perrefort <djperrefort@gmail.com>
2026-08-18 10:43:58 -0400

    Displays default values in cli help text

--- jh_config_manager/cli.py
+++ jh_config_manager/cli.py
@@ -10,6 +10,11 @@ import os
 
 __all__ = ['create_parser']
 
+DEFAULT_VENV_ROOT = '/ihome/crc/install/jupyterhub/hub.5.2.1/envs'
+DEFAULT_MODULES_CONFIG_FILE = '/ihome/crc/install/jupyterhub/modules_config.json'
+DEFAULT_CACHE_FILE = '/ihome/crc/install/jupyterhub/config_cache.json'
+DEFAULT_RELOAD_INTERVAL = 30
+
 
 def create_parser() -> argparse.ArgumentParser:
     """Build the command-line argument parser for the config manager service.
@@ -18,16 +23,43 @@ def create_parser() -> argparse.ArgumentParser:
         The configured argument parser.
     """
 
-    default_venv_root = os.environ.get('VENV_ROOT', '/ihome/crc/install/jupyterhub/hub.5.2.1/envs')
-    default_modules_config_file = os.environ.get('MODULES_CONFIG_FILE', '/ihome/crc/install/jupyterhub/modules_config.json')
-    default_cache_file = os.environ.get('CACHE_FILE', '/ihome/crc/install/jupyterhub/config_cache.json')
-    default_reload_interval = int(os.environ.get('RELOAD_INTERVAL', 30))
+    default_venv_root = os.environ.get('VENV_ROOT', DEFAULT_VENV_ROOT)
+    default_modules_config_file = os.environ.get('MODULES_CONFIG_FILE', DEFAULT_MODULES_CONFIG_FILE)
+    default_cache_file = os.environ.get('CACHE_FILE', DEFAULT_CACHE_FILE)
+    default_reload_interval = int(os.environ.get('RELOAD_INTERVAL', DEFAULT_RELOAD_INTERVAL))
 
-    parser = argparse.ArgumentParser(description="JupyterHub config manager service")
+    parser = argparse.ArgumentParser(
+        description="JupyterHub config manager service",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
 
-    parser.add_argument('--venv_root', default=default_venv_root)
-    parser.add_argument('--modules_config_file', default=default_modules_config_file)
-    parser.add_argument('--cache_file', default=default_cache_file)
-    parser.add_argument('--reload_interval', type=int, default=default_reload_interval)
+    parser.add_argument(
+        '-v', '--venv_root',
+        default=default_venv_root,
+        metavar='PATH',
+        help='Path to the directory containing virtualenv subdirectories.',
+    )
+
+    parser.add_argument(
+        '-m', '--modules_config_file',
+        default=default_modules_config_file,
+        metavar='PATH',
+        help='Path to the JSON file describing available modules.',
+    )
+
+    parser.add_argument(
+        '-c', '--cache_file',
+        default=default_cache_file,
+        metavar='PATH',
+        help='Path to the cache file consumed by the JupyterHub spawner.',
+    )
+
+    parser.add_argument(
+        '-r', '--reload_interval',
+        type=int,
+        default=default_reload_interval,
+        metavar='SECONDS',
+        help='Number of seconds to wait between scans.',
+    )
 
     return parser
